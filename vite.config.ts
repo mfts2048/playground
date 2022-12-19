6cc0716f74bcfc5318be32b9baf01fc79fe78d81
yxswy <18267094443@163.com>
2022-12-19 10:24:59 +0800

    修改打包产物

--- vite.config.ts
+++ vite.config.ts
@@ -6,21 +6,21 @@ import { resolve, join } from "path";
 export default defineConfig({
 	plugins: [vue()],
 	base: "/playground",
-	build: {
-		lib: {
-			entry: join(resolve(), "lib/index.ts"),
-			name: "MyLib",
-			fileName: "my-lib",
-		},
-		rollupOptions: {
-			// 确保外部化处理那些你不想打包进库的依赖
-			external: ["vue"],
-			output: {
-				// 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-				globals: {
-					vue: "Vue",
-				},
-			},
-		},
-	},
+	// build: {
+	// 	lib: {
+	// 		entry: join(resolve(), "lib/index.ts"),
+	// 		name: "MyLib",
+	// 		fileName: "my-lib",
+	// 	},
+	// 	rollupOptions: {
+	// 		// 确保外部化处理那些你不想打包进库的依赖
+	// 		external: ["vue"],
+	// 		output: {
+	// 			// 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+	// 			globals: {
+	// 				vue: "Vue",
+	// 			},
+	// 		},
+	// 	},
+	// },
 });
