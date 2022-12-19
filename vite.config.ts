d492a40883ab6d43c089c77212759b58b35da3a2
yxswy <18267094443@163.com>
2022-12-19 10:19:25 +0800

    测试1

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve, join } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	base: "/playground",
 	build: {
 		lib: {
 			entry: join(resolve(), "lib/index.ts"),
